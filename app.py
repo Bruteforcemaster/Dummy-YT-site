7206c39ebb512258065187e7afae3ec2ad13bb9f
Soumya Bera <87431020+Bruteforcemaster@users.noreply.github.com>
2023-02-21 19:37:37 +0000

    Added Open position

--- app.py
+++ app.py
@@ -2,9 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {
+    'id': 1,
+    'title': "Data Analyst",
+    'location': "Bangaluru, India",
+    'salary': "Rs. 1000000"
+  },
+  {
+    'id': 2,
+    'title': "Data Scientist",
+    'location': "Delhi, India",
+    'salary': "Rs. 2000000"
+  },
+{
+    'id': 3,
+    'title': "Frontend Engineer",
+    'location': "Remote",
+    'salary': "Rs. 1200000"
+  },
+{
+    'id': 4,
+    'title': "Backend Engineer",
+    'location': "San Fransisco, USA",
+    'salary': "$120,000"
+  }
+]
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS)
 
 
 
